d02b1ae934010d83c4d823e93587406e28f2b9b5
Glitch (hello-webpage) <none>
2019-07-17 18:10:22 +0000

    🐟🏨 Checkpoint ./index.html:3813951/72 ./style.css:3813951/71 ./script.js:3813951/905

--- script.js
+++ script.js
@@ -5,29 +5,73 @@
 let firstNames = [
   "Tater",
   "Chanum",
+  "Channing",
   "Tasty",
   "Tater Tot",
   "Chancey",
   "Tastey",
   "Turgid",
-  "Tiny Tim",
-  "Tum Tum",
   "Churlish",
   "Mark Wahlberg",
-  "Choo Choo"
+  "Choo Choo",
+  "Chandelier",
+  "Chutney",
+  "Hamming",
+  "Hammy",
+  "Toe Jam",
+  "Chinning",
+  "Toe Jam",
+  "Chimney",
+  "Charlie's",
+  "Possum",
+  "Chitty Chitty",
+  "Curly",
+  "Chowder",
+  "Chuggachugga",
+  "Poodle",
+  "Chunnel",
+  "Jittery",
+  "Chunky",
+  "Chunder",
+  "Lady Chatterley's",
+  "Chat",
+  "Charming",
+  "Chanting"
 ]
 
 let lastNames = [
-  "Chanting",
-  "Chancey",
   "Tainting",
+  "Tiny Tim",
+  "Tum Tum",
   "Chanson",
   "Channel",
   "Tater Tot",
   "Turtle Toy",
   "Tates",
   "Toodles",
-  "Train Train"
+  "Train Train",
+  "Tangles",
+  "Chattanooga",
+  "Tang Tang",
+  "Trawler",
+  "Totes",
+  "Taterball",
+  "Tamper",
+  "Tablet",
+  "Turkey",
+  "Tootin'",
+  "Mankell",
+  "Tantrum",
+  "Titter Tada",
+  "Tang Bang",
+  "Tada",
+  "Tube Top",
+  "TOOT TOOT!",
+  "Muscle",
+  "Atom",
+  "Tilts",
+  "Tatum",
+  "Chatum"
 ]
 
 function soRandom(items) {
@@ -36,9 +80,8 @@ function soRandom(items) {
 
 function generateName(){
     $("#first-name-display").text(firstNames[soRandom(firstNames.length)])
-    $("#last-name-display").text(firstNames[soRandom(firstNames.length)])
+    $("#last-name-display").text(lastNames[soRandom(lastNames.length)])
 }
-
 
 $(document).ready(function() {
     generateName()
