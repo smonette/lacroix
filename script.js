9eebc43733a1be19f85ca5d5ab404abb7901461f
Glitch (hello-webpage) <none>
2019-07-17 17:59:13 +0000

    🗽🌽 Checkpoint ./index.html:3813951/527 ./script.js:3813951/1415

--- script.js
+++ script.js
@@ -2,4 +2,48 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-console.log('hi');
+let firstNames = [
+  "Tater",
+  "Chanum",
+  "Tasty",
+  "Tater Tot",
+  "Chancey",
+  "Tastey",
+  "Turgid",
+  "Tiny Tim",
+  "Tum Tum",
+  "Churlish",
+  "Mark Wahlberg",
+  "Choo Choo"
+]
+
+let lastNames = [
+  "Chanting",
+  "Chancey",
+  "Tainting",
+  "Chanson",
+  "Channel",
+  "Tater Tot",
+  "Turtle Toy",
+  "Tates",
+  "Toodles",
+  "Train Train"
+]
+
+function soRandom(items) {
+  return Math.floor(Math.random() * (items))
+}  
+
+function generateName(){
+    $("#first-name-display").text(firstNames[soRandom(firstNames.length)])
+    $("#last-name-display").text(firstNames[soRandom(firstNames.length)])
+}
+
+
+$(document).ready(function() {
+    generateName()
+});
+
+$( "#generate-name-btn" ).click(function() {
+  generateName()
+});
