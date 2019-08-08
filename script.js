7846ce53c2a3b1671901b7ce0d76870eefaf7a4d
Glitch (channing-tatum) <none>
2019-08-08 19:30:23 +0000

    🚦👨‍❤️‍💋‍👨 Checkpoint ./script.js:3813951/832 ./index.html:3813951/44 ./style.css:3813951/78

--- script.js
+++ script.js
@@ -34,14 +34,32 @@ let flavors = [
   "Unicorn"
 ]
 
+let colors = [
+  "#5f02e7",
+  "#39d6f9",
+  "#e72b6d",
+  "#73cda4",
+  "#772485",
+  "#976029",
+  "#3cca2c",
+  "#febf08",
+  "#e2cf9a"
+]
+
 function soRandom(items) {
-  return Math.floor(Math.random() * (items))
+  return Math.floor(Math.random() * (items.length))
 }  
 
+function randomColor(){
+  let selectedColor = colors[soRandom(colors)]
+   $('#name-display').css("color", selectedColor)
+}
+
 function generateName(){
- let firstFlavor = flavors[soRandom(flavors.length)]
- let secondFlavor = flavors[soRandom(flavors.length)]
+ let firstFlavor = flavors[soRandom(flavors)]
+ let secondFlavor = flavors[soRandom(flavors)]
  $('#name-display').text(`${firstFlavor}-${secondFlavor}`)
+ randomColor()
 }
 
 
